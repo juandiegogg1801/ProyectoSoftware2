0a5e844eaf370db0ca2a0c66e05ee2d736fc65eb
juandiegogg1801@gmail.com <juandiegogg1801@gmail.com>
2024-07-06 12:56:19 -0500

    Proyecto springboot agregado

---
+++ spring-boot-backend-gymUPTC/src/main/java/co/edu/uptc/Software2/springboot/backend/gymUPTC/SpringBootBackendGymUptcApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.Software2.springboot.backend.gymUPTC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendGymUptcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendGymUptcApplication.class, args);
+    }
+
+}
---
+++ spring-boot-backend-gymUPTC/src/test/java/co/edu/uptc/Software2/springboot/backend/gymUPTC/SpringBootBackendGymUptcApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.Software2.springboot.backend.gymUPTC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendGymUptcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
